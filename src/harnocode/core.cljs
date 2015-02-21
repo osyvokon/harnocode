058bee490b03e34f67df5da6867697e34854a507
Aleksey Sivokon <aleksey.sivokon@gmail.com>
2015-02-21 13:57:15 +0200

    Create empty figwheel project

---
+++ src/harnocode/core.cljs
@@ -0,0 +1,12 @@
+(ns ^:figwheel-always harnocode.core
+    (:require))
+
+(enable-console-print!)
+
+(println "Edits to this text should show up in your developer console.")
+
+;; define your app data so that it doesn't get over-written on reload
+
+(defonce app-state (atom {:text "Hello world!"}))
+
+
