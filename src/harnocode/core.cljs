f4247b3cfd81230f073b7b55e1f7f743835c1767
Aleksey Sivokon <aleksey.sivokon@gmail.com>
2015-02-21 14:16:40 +0200

    Dummy code tokenizer.

--- src/harnocode/core.cljs
+++ src/harnocode/core.cljs
@@ -1,12 +1,22 @@
 (ns ^:figwheel-always harnocode.core
-    (:require))
+    (:require [goog.dom :as dom]))
 
 (enable-console-print!)
 
-(println "Edits to this text should show up in your developer console.")
+(println "edits to this text should show up in your developer console.")
 
 ;; define your app data so that it doesn't get over-written on reload
 
 (defonce app-state (atom {:text "Hello world!"}))
+
+(defn show-harnocode! [harnocode]
+  (let [output-area (dom/getElement "output")]
+    (set! (.-value output-area) harnocode)))
+
+(let [code "var answer = 42;"
+      harnocode (js/esprima.tokenize code)]
+  (println harnocode)
+  (show-harnocode! harnocode))
+
 
 
