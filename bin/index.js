f30aac80c2226169fb2bcbe3cbdcadc32a882108
Oleksiy Syvokon <oleksiy.syvokon@gmail.com>
2021-12-06 16:17:42 +0200

    Command-line arguments

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,24 @@
 #!/usr/bin/env node
-let harnocode = require('../harnocode.js');
-harnocode.formatFile("./gantt.module.js"); // TODO
+const harnocode = require('../harnocode.js');
+const commandLineArgs = require('command-line-args');
+const commandLineUsage = require('command-line-usage');
+
+const optionsDefs = [
+  { name: "input", type: String, defaultOption: true },
+  { name: "skip-validation", type: Boolean, help: "Do not validate harnified code"}
+]
+const sections = [
+  {
+    header: 'Options',
+    optionList: optionsDefs
+  }
+]
+const options = commandLineArgs(optionsDefs);
+
+if (!options.input) {
+  const usage = commandLineUsage(sections);
+  process.stdout.write(usage);
+  process.exit(-1);
+}
+
+harnocode.formatFile(options.input, options);
