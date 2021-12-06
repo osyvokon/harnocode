e6d7242075b264b566c61340f0b46d0a3ef00106
Oleksiy Syvokon <oleksiy.syvokon@gmail.com>
2021-12-06 17:10:22 +0200

    Read mask from a text file

--- bin/index.js
+++ bin/index.js
@@ -2,9 +2,11 @@
 const harnocode = require('../harnocode.js');
 const commandLineArgs = require('command-line-args');
 const commandLineUsage = require('command-line-usage');
+const fs = require("fs");
 
 const optionsDefs = [
   { name: "input", type: String, defaultOption: true },
+  { name: "mask", type: String },
   { name: "skip-validation", type: Boolean, help: "Do not validate harnified code"}
 ]
 const sections = [
@@ -15,10 +17,34 @@ const sections = [
 ]
 const options = commandLineArgs(optionsDefs);
 
-if (!options.input) {
+if (!options.input && !options.mask) {
   const usage = commandLineUsage(sections);
   process.stdout.write(usage);
   process.exit(-1);
 }
 
-harnocode.formatFile(options.input, options);
+let mask;
+if (!options.mask) {
+  mask = `\
+xxxxxxxxxx           xxxxxxxxxxx      xxxxxxxxxx
+ xxxxxxxxxx           xxxxxxxxxxx      xxxxxxxxxx
+  xxxxxxxxxx           xxxxxxxxxxx      xxxxxxxxxx
+   xxxxxxxxxx           xxxxxxxxxxx      xxxxxxxxxx
+    xxxxxxxxxx           xxxxxxxxxxx      xxxxxxxxxx
+     xxxxxxxxxx           xxxxxxxxxxx      xxxxxxxxxx
+      xxxxxxxxxx           xxxxxxxxxxx      xxxxxxxxxx
+       xxxxxxxxxx           xxxxxxxxxxx      xxxxxxxxxx
+       xxxxxxxxxx           xxxxxxxxxxx      xxxxxxxxxx
+      xxxxxxxxxx           xxxxxxxxxxx      xxxxxxxxxx
+     xxxxxxxxxx           xxxxxxxxxxx      xxxxxxxxxx
+    xxxxxxxxxx           xxxxxxxxxxx      xxxxxxxxxx
+   xxxxxxxxxx           xxxxxxxxxxx      xxxxxxxxxx
+  xxxxxxxxxx           xxxxxxxxxxx      xxxxxxxxxx
+ xxxxxxxxxx           xxxxxxxxxxx      xxxxxxxxxx
+xxxxxxxxxx           xxxxxxxxxxx      xxxxxxxxxx
+`.repeat(10000);
+} else {
+  mask = fs.readFileSync(options.mask).toString();
+}
+
+harnocode.formatFile(options.input, mask, options);
